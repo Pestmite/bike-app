b4d306f503b2da39d2b938b38e640fd0d6c62eac
Pestmite <Charleshenrichaki@gmail.com>
2025-07-21 11:12:24 -0400

    Created routing system

--- scripts/map.js
+++ scripts/map.js
@@ -1,6 +1,6 @@
-import { MAP_API } from '../keys.js';
+import { MAP_API, ROUTE_KEY } from '../keys.js';
 
-const cycleMap = `https://tile.thunderforest.com/cycle/{z}/{x}/{y}.png?apikey=${MAP_API}`
+const atlasMap = `https://tile.thunderforest.com/atlas/{z}/{x}/{y}.png?apikey=${MAP_API}`
 
 const body = document.querySelector('.body');
 body.innerHTML = `
@@ -9,9 +9,30 @@ body.innerHTML = `
   </div>
 `;
 
-const map = L.map('map').setView([45.5019, -73.5674], 13); 
-
-L.tileLayer(cycleMap, {
-  maxZoom: 22,
-  attribution: '&copy; <a href="https://www.thunderforest.com/">Thunderforest</a>, OpenStreetMap contributors',
+const map = L.map('map', { attributionControl:false }).setView([45.5019, -73.5674], 13); 
+console.log(L.Routing.OpenRouteServiceV2)
+L.tileLayer(atlasMap, {
+  maxZoom: 20,
+  minZoom: 2
 }).addTo(map);
+
+const start = L.latLng(45.5019, -73.5674);
+
+let routingControl;
+
+map.on('click', function (e) {
+  const end = e.latlng;
+
+  if (routingControl) {
+    map.removeControl(routingControl);
+  } 
+
+  routingControl = L.Routing.control({
+    waypoints: [start, end],
+    showAlternatives: true,
+    routeWhileDragging: true,
+    router: new L.Routing.OpenRouteServiceV2(ROUTE_KEY, {
+      profile: 'cycling-regular',
+    }),
+  }).addTo(map);
+});
